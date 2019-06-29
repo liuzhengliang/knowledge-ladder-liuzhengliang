5763cd5561af659b4d112ecf4a88bb066fe7cf47
刘政良 <liuzhengliang@icourt.cc>
2019-06-29 17:01:26 +0800

    11.开发您的第一个Spring Boot应用程序-第二部分。入门

--- src/main/java/com/liuzhengliang/web/controller/HelloController.java
+++ src/main/java/com/liuzhengliang/web/controller/HelloController.java
@@ -12,9 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World";
-    }
-
 }
